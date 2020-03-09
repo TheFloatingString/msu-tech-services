5c1df5c6db11384dbb87b01d614c5dcb669b372d
The Floating String <laurencel2001@gmail.com>
2020-03-08 22:06:36 -0400

    Add template rendering for saves.html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return "home"
+
+@app.route("/saves")
+def saves():
+	return render_template("saves.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
